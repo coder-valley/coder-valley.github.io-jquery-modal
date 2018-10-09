f2bf44707b72ed28b34e32c9a161833b26b4c735
Ashutosh Singh <ashu5oct1994@gmail.com>
2018-10-09 17:23:07 +0530

    Add files via upload

---
+++ temple.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+	
+	$('#subpro').click(()=>{
+		$("#allpro").show();
+		$('#veggies').hide();
+		$('#fruits').hide();
+		$('#grocery').hide();
+	})
+	$('#veggies').hide();
+	$('#veg-sub').click(()=>{
+		$('#veggies').show();
+		$("#allpro").hide();
+		$('#fruits').hide();
+		$('#grocery').hide();
+	})
+	$('#fruits').hide()
+	$('#sub-fru').click(()=>{
+		$('#fruits').show();
+		$('#veggies').hide();
+		$("#allpro").hide();
+		$('#grocery').hide();
+	})
+	$('#grocery').hide();
+	$('#groc-sub').click(()=>{
+		$('#grocery').show();
+		$('#fruits').hide();
+		$('#veggies').hide();
+		$("#allpro").hide();
+	})
+})
